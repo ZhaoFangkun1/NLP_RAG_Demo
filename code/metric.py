a320697fd4f6b7e8f5c06a1192610195f63e67f8
Chipp <94703109+emt0re0@users.noreply.github.com>
2024-06-18 14:57:26 +0800

    Update metric.py for add bertscore

--- code/metric.py
+++ code/metric.py
@@ -8,6 +8,7 @@ import json
 
 import nltk
 from rouge import Rouge
+from bert_score import score
 
 
 def metric(pred: str, answer_list: list[str]):
@@ -20,14 +21,27 @@ def metric(pred: str, answer_list: list[str]):
     # 计算ROUGE
     rouge = Rouge()
     rouge_scores = rouge.get_scores(pred, ' '.join(answer_list))
+    
+     # 确保pred和每个答案都是列表形式
+    preds = [pred.lower().strip()]
+    refs = [answer.lower().strip() for answer in answer_list]
+    
+    # 计算BERTScore的精确度、召回率和F1分数
+    bs = score(preds, refs, model_type='bert-base-uncased', num_layers=12, idf=False, device= 'cuda')
+    
+    # BERTScore返回的是一个包含三个元素的元组，分别对应精确度、召回率和F1分数
+    bert_p, bert_r, bert_f = bs
 
-    return bleu_score, rouge_scores[0]['rouge-l']
+    return bleu_score, rouge_scores[0]['rouge-l'], bert_p, bert_r, bert_f
 
 
 if __name__ == '__main__':
     with open('../data/crag_200_result.jsonl', 'r') as f:
         lines = f.readlines()
     bleu_scores = []
+    bert_p_scores = []
+    bert_r_scores = []
+    bert_f_scores = []
     rouge_l_p = []
     rouge_l_r = []
     rouge_l_f = []
@@ -36,13 +50,20 @@ if __name__ == '__main__':
         query = data['query']
         answer = data['answer']
         pred = data['pred']
-        bleu, rouge_l = metric(pred.lower().strip(), [answer.lower().strip()])
+        bleu, rouge_l, bert_p, bert_r, bert_f = metric(pred.lower().strip(), [answer.lower().strip()])
         bleu_scores.append(bleu)
         rouge_l_p.append(rouge_l['p'])
         rouge_l_r.append(rouge_l['r'])
         rouge_l_f.append(rouge_l['f'])
+        bert_p_scores.append(bert_p)
+        bert_r_scores.append(bert_r)
+        bert_f_scores.append(bert_f)
 
     print(f"BLEU: {sum(bleu_scores) / len(bleu_scores)}")
     print(f"ROUGE-L P: {sum(rouge_l_p) / len(rouge_l_p)}")
     print(f"ROUGE-L R: {sum(rouge_l_r) / len(rouge_l_r)}")
     print(f"ROUGE-L F: {sum(rouge_l_f) / len(rouge_l_f)}")
+    print(f"BERTScore P: {sum(bert_p_scores) / len(bert_p_scores)}")
+    print(f"BERTScore R: {sum(bert_r_scores) / len(bert_r_scores)}")
+    print(f"BERTScore F: {sum(bert_f_scores) / len(bert_f_scores)}")
+
